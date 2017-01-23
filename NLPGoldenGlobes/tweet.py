9c742edb0b6bd3a3997e28d5186fd2c4455ec5d7
LuoLei_Zhao <LuoLei_Zhao>
2017-01-23 17:00:56 -0600

    Added tweet class

---
+++ NLPGoldenGlobes/tweet.py
@@ -0,0 +1,15 @@
+#A General Python Class intended to represent the contents of a single tweet
+
+# Format of a tweet:
+# Text: Contents of the tweet as a string
+# 
+from datetime import datetime
+
+class Tweet:
+	def __init__(self,unparsed, content,authorID,authorName,tweetID, dateTime):
+		self.unparsed = unparsed
+		self.content = content
+		self.authorID = authorID
+		self.authorName = authorName
+		self.tweetID = tweetID
+		self.dateTime = datetime.strptime(dateTime,'%Y-%m-%d %H:%M:%S')
