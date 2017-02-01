c13f3364cfc94fce5b0d270eccefd9fec39b0d8a
Pierce Gleeson <piercegleeson@gmail.com>
2017-02-01 17:31:07 -0600

    nicer authentication

--- NLPGoldenGlobes/twitter_handles.py
+++ NLPGoldenGlobes/twitter_handles.py
@@ -2,24 +2,33 @@ import twitter
 import os
 import re
 
-#need twitter auth keys
-#you will need a twitter account for this
-#make app at apps.twitter.com
-#then create access token under keys and access tokens
-api = twitter.Api(consumer_key=os.environ.get('TW_CONSUMER_KEY'),
-                  consumer_secret=os.environ.get('TW_CONSUMER_SECRET'),
-                  access_token_key=os.environ.get('TW_GGNLP_ACCESS_TOKEN'),
-                  access_token_secret=os.environ.get('TW_GGNLP_ACCESS_TOKEN_SECRET'))
-
-
+def authenticate():
+    #need twitter auth keys
+    #you will need a twitter account for this
+    #make app at apps.twitter.com
+    #then create access token under keys and access tokens
+    api = twitter.Api(consumer_key=os.environ.get('TW_CONSUMER_KEY'),
+                      consumer_secret=os.environ.get('TW_CONSUMER_SECRET'),
+                      access_token_key=os.environ.get('TW_GGNLP_ACCESS_TOKEN'),
+                      access_token_secret=os.environ.get('TW_GGNLP_ACCESS_TOKEN_SECRET'))
+    try:
+        api.VerifyCredentials()
+    except twitter.error.TwitterError:
+        print 'Could not authenticate user. Verify that the proper env variables are set.'
+        print 'TW_CONSUMER_KEY\nTW_CONSUMER_SECRET\nTW_GGNLP_ACCESS_TOKEN\nTW_GGNLP_ACCESS_TOKEN_SECRET'
+        return 0
+    return api
 
 #print(api.GetUser(screen_name='@rkrkrklyly'))
 
 def get_user_info(handle):
+    api = authenticate()
+    if not api:
+        return ''
     try:
         user_prof = api.GetUser(screen_name=handle)
     except twitter.error.TwitterError:
-        return 0
+        return ''
     user_prof = user_prof.AsDict()
     name = user_prof[u'name']
     followers = user_prof[u'followers_count']
@@ -33,6 +42,6 @@ def remove_movie(name):
 def correct_capitalization(name):
     return name.title()
 
-movie_name, movie_followers = get_user_info('@moonlightmov')
-print remove_movie(movie_name)
-print correct_capitalization(movie_name)
+#movie_name, movie_followers = get_user_info('@moonlightmov')
+#print remove_movie(movie_name)
+#print correct_capitalization(movie_name)
