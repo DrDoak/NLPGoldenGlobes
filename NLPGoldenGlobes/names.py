b67f8f0217f3e024bf9408f5d123135cf9b74ce9
Ben Meline <benmeline@gmail.com>
2017-02-01 17:16:59 -0600

    Most frequent name

--- NLPGoldenGlobes/names.py
+++ NLPGoldenGlobes/names.py
@@ -1,6 +1,18 @@
 import nltk
 import re
 
+def most_frequent_name(tweets):
+  names_dict = count_names(tweets)
+  return max(names_dict.iterkeys(), key=lambda key: names_dict[key])
+
+def count_names(tweets):
+  names_dict = {}
+  for tweet in tweets:
+    names = extract_names(tweet.content)
+    for name in names:
+      names_dict[name] = names_dict.get(name, 0) + 1
+  return names_dict  
+
 def extract_names(text):
   names = []
   for sent in nltk.sent_tokenize(text):
