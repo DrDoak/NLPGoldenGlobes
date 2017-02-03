5c3bc9ef2db99d7b56e647dbe4542be40758e3a1
LuoLei_Zhao <LuoLei_Zhao>
2017-02-03 14:45:25 -0600

    Added tweet filtering system

--- NLPGoldenGlobes/tweet.py
+++ NLPGoldenGlobes/tweet.py
@@ -3,7 +3,12 @@
 # Format of a tweet:
 # Text: Contents of the tweet as a string
 # 
+import nltk
 from datetime import datetime
+
+bannedWords = ["RT","http","https","to","the"]
+bannedPrefixes = ["//"]
+
 
 class Tweet:
   def __init__(self,unparsed, content,authorID,authorName,tweetID, dateTime):
@@ -15,6 +20,8 @@ class Tweet:
     self.dateTime = datetime.strptime(dateTime,'%Y-%m-%d %H:%M:%S')
     self.tf = {}
     self.tfidf = {}
+    self.tokens = []
+    self.createTokens()
 
   def has_word(self, targetWord):
     words = self.content.split(' ')
@@ -22,3 +29,12 @@ class Tweet:
       if word == targetWord:
         return True
     return False
+
+  def createTokens(self):
+    global bannedPrefixes, bannedWords
+    candidateTokens = nltk.word_tokenize(self.content)
+    for token in candidateTokens:
+      if len(token) > 1 and not token in bannedWords:
+        for testPrefix in bannedPrefixes:
+          if not token[0:len(testPrefix)] == testPrefix:
+            self.tokens.append(token)
