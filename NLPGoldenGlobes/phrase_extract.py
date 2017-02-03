054531eb2bd560c892dbef2dbc6eec5e2c814f13
Jonathan Kim <jonathankim2018@u.northwestern.edu>
2017-02-02 18:41:19 -0600

    updated to use regex and query filtering

--- NLPGoldenGlobes/phrase_extract.py
+++ NLPGoldenGlobes/phrase_extract.py
@@ -3,11 +3,13 @@ from nltk.util import ngrams
 import operator
 from parser import parser
 import string
+import regex
 
 def test(inputFileName):
 	tweets = parser(inputFileName)
-	tweets = tweets[:10000]
-	return extract_movie_titles(tweets)
+	mp=regex.any_match(tweets,[r'(?i)\bmotion picture\b'])
+	comedy=regex.any_match(mp,[r'(?i)\banimated\b'])
+	return extract_movie_titles(comedy)
 	
 
 
@@ -15,10 +17,13 @@ def test(inputFileName):
 def extract_movie_titles(tweets):
 	possible_movie_titles= []
 	for i in range(2,5):
-		list = extract_phrase_count(tweets, i)
+		dict = extract_phrase_count(tweets, i)
+		dict = remove_punctuation(dict)
+		queries = ['Best', 'Motion', 'Picture', 'Drama', 'or' , 'in', 'a', 'actor', 'best', 'motion', 'actress']
+		dict = remove_query_type(dict, queries)
 		for i in range(5):
-			temp_1 = max(list.iterkeys(), key = lambda key: list[key])
-			list.pop(temp_1, None)
+			temp_1 = max(dict.iterkeys(), key = lambda key: dict[key])
+			dict.pop(temp_1, None)
 			possible_movie_titles.append(temp_1)
 	return possible_movie_titles
 		
@@ -35,7 +40,7 @@ def extract_phrase_count(tweets, length):
 				dict[phrase] +=1
 			else:
 				dict[phrase] = 1
-	return remove_punctuation(dict)
+	return dict
 
 #removes phrases that have punctuation; presumably phrases with punctuation
 #are not movie titles
@@ -49,6 +54,15 @@ def remove_punctuation(dict):
 		dict.pop(i, None)
 	return dict
 		
-	
+def remove_query_type(dict, queries):
+	remove = []	
+	for phrase in dict:
+		for word in phrase:
+			if word in queries:
+				remove.append(phrase)
+	for i in remove:
+		dict.pop(i, None)
+	return dict
+		
 		
 		
