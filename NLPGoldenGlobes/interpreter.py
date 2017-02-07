7244910b6e5279b556007098474bbd90548efd30
Ben Meline <benmeline@gmail.com>
2017-02-07 15:35:53 -0600

    Exclude tweets that mention other queries

--- NLPGoldenGlobes/interpreter.py
+++ NLPGoldenGlobes/interpreter.py
@@ -7,11 +7,16 @@ ngram_threshold = 0.75
 
 def get_winners(tweets, queries):
   results = []
+  queries_set = create_queries_set(queries)
 
   for query in queries:
     matched_tweets = regex.all_match(tweets, query.get_patterns())
-    unigrams = phrases.extract_unigrams(matched_tweets)
-    ngrams = phrases.extract_ngrams(matched_tweets)
+    other_queries = queries_set.difference(query.tokens)
+    other_queries_patterns = regex.create_patterns(other_queries)
+    matched_tweets_minus_other_queries = regex.exclude_any_match(matched_tweets, other_queries_patterns)
+
+    unigrams = phrases.extract_unigrams(matched_tweets_minus_other_queries)
+    ngrams = phrases.extract_ngrams(matched_tweets_minus_other_queries)
 
     phrases.remove_query(unigrams, query)
     phrases.remove_query(ngrams, query)
@@ -42,3 +47,9 @@ def get_winners(tweets, queries):
       results.append(Result(query.unparsed, winner))
 
   return results
+
+def create_queries_set(queries):
+  queries_set = set()
+  for query in queries:
+    queries_set.update(query.tokens)
+  return queries_set
