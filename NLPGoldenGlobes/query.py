0b599939e2d7aedab06328f1a98f007875290c32
Ben Meline <benmeline@gmail.com>
2017-02-06 19:03:57 -0600

    Get winner for every query

--- NLPGoldenGlobes/query.py
+++ NLPGoldenGlobes/query.py
@@ -1,5 +1,12 @@
+import re
 
 class Query:
   def __init__(self,unparsed, tokens):
     self.unparsed = unparsed
     self.tokens = tokens
+
+  def get_patterns(self):
+    patterns = []
+    for token in self.tokens:
+      patterns.append(r'(?i)\b' + re.escape(token) + r'\b')
+    return patterns
