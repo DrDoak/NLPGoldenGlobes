b7db1b3cd4cd01a0cb2ff95e5ab39caee543bce6
Ben Meline <benmeline@gmail.com>
2017-02-09 09:22:59 -0600

    Remove old functions

--- NLPGoldenGlobes/interpreter.py
+++ NLPGoldenGlobes/interpreter.py
@@ -4,54 +4,10 @@ from collections import Counter
 from result import Result
 import string
 
-ngram_threshold = 0.75
 xgram_threshold = 0.49
 mention_hashtag_weight = 0.15
 
 def get_winners(tweets, queries):
-  results = []
-  queries_set = create_queries_set(queries)
-
-  for query in queries:
-    matched_tweets = regex.all_match(tweets, query.get_patterns())
-    other_queries = queries_set.difference(query.tokens)
-    other_queries_patterns = regex.create_patterns(other_queries)
-    matched_tweets_minus_other_queries = regex.exclude_any_match(matched_tweets, other_queries_patterns)
-
-    unigrams = phrases.extract_unigrams(matched_tweets_minus_other_queries)
-    ngrams = phrases.extract_ngrams(matched_tweets_minus_other_queries)
-
-    phrases.remove_query(unigrams, query)
-    phrases.remove_query(ngrams, query)
-
-    phrases.remove_stopwords(unigrams)
-    phrases.remove_stopwords(ngrams)
-
-    phrases.remove_twitter_stopwords(unigrams)
-    phrases.remove_twitter_stopwords(ngrams)
-
-    unigrams_count = Counter(unigrams)
-    ngrams_count = Counter(ngrams)
-
-    if unigrams_count and ngrams_count:
-      max_unigram = unigrams_count.most_common(1)[0]
-      max_ngram = ngrams_count.most_common(1)[0]
-
-      max_unigram_phrase = max_unigram[0][0]
-      max_unigram_count = max_unigram[1]
-
-      max_ngram_phrase = max_ngram[0]
-      max_ngram_count = max_ngram[1]
-
-      if (max_unigram_phrase in max_ngram_phrase) or (max_ngram_count > ngram_threshold * max_unigram_count):
-        winner = ' '.join(max_ngram_phrase)
-      else:
-        winner = max_unigram_phrase
-      results.append(Result(query.unparsed, winner))
-
-  return results
-
-def get_winners_xgrams(tweets, queries):
   results = []
   queries_set = create_queries_set(queries)
 
@@ -91,15 +47,6 @@ def get_winners_xgrams(tweets, queries):
       results.append(Result(query.unparsed, winner))
 
   return results
-
-def is_subset(xgrams,ygrams):
-  for x in xgrams:
-    xgram_phrase = ' '.join(x[0][0])
-    for y in ygrams:
-      ygram_phrase = ' '.join(y[0][0])
-      if xgram_phrase in ygram_phrase:
-        return True
-  return False
 
 def create_queries_set(queries):
   queries_set = set()
--- NLPGoldenGlobes/main.py
+++ NLPGoldenGlobes/main.py
@@ -4,7 +4,7 @@ import interpreter
 def run_queries(tweets_file, queries_file):
   tweets = parser.parse_tweets(tweets_file)
   queries = parser.parse_queries(queries_file)
-  winners = interpreter.get_winners_xgrams(tweets, queries)
+  winners = interpreter.get_winners(tweets, queries)
 
   for winner in winners:
     winner.prettyprint()
